2c99b57a92abe5c696975f7c6c6411035c4a053a
Ashmit Khadka <a0khadka@gmail.com>
2025-02-04 23:31:19 +0000

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router'
-import { ToastContainer, toast } from 'react-toastify';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import HomeScreen from './components/screens/HomeScreen';
 import ArticlesScreen from './components/screens/ArticlesScreen';
 import CommitteeScreen from './components/screens/CommitteeScreen';
 import ArticleScreen from './components/screens/ArticleScreen';
-
 import Navigation from './components/Navigation';
 import Login from './components/admin/Login';
 import List from './components/admin/List';
@@ -22,44 +20,31 @@ import { LoginProvider } from './LoginContext';
 
 function App() {
   return (
-      <LoginProvider>
-        <div className='flex flex-col items-center pt-24'>
-        <ToastContainer />
-          <Navigation />
-          <div className='px-4 md:px-0 w-full flex flex-col items-center'>
-            <Routes>
-              <Route index element={<HomeScreen />} />
-              <Route path="projects" element={<ArticlesScreen />} />
-              <Route path="committee" element={<CommitteeScreen />} />
-              <Route path="article/:id" element={<ArticleScreen />} />
-              <Route path="minutes" element={<MinutesScreen />} />
-              <Route path="events" element={<EventsScreen />} />
-              <Route path="admin" element={<Login />} />
-              <Route path="about" element={<AboutScreen />} />
-              <Route element={<ProtectedRoute />}>
-                <Route path="admin/dashboard/:type" element={<List />} />
-                <Route path="admin/article" element={<ArticleForm />} />
-                <Route path="admin/member" element={<MemberForm />} />
-                <Route path="admin/minute" element={<MinuteForm />} />
-                <Route path="admin/event" element={<EventForm />} />
-              </Route>
-              {/* <Route element={<AuthLayout />}>
-              <Route path="login" element={<Login />} />
-              <Route path="register" element={<Register />} />
+    <LoginProvider>
+      <div className='flex flex-col items-center pt-24'>
+        <Navigation />
+        <div className='max-w-page margin-auto px-4 md:px-0'>
+          <Routes>
+            <Route index element={<HomeScreen />} />
+            <Route path="articles" element={<ArticlesScreen />} />
+            <Route path="committee" element={<CommitteeScreen />} />
+            <Route path="article/:id" element={<ArticleScreen />} />
+            <Route path="minutes" element={<MinutesScreen />} />
+            <Route path="events" element={<EventsScreen />} />
+            <Route path="admin" element={<Login />} />
+            <Route path="about" element={<AboutScreen />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path="admin/dashboard" element={<List />} />
+              <Route path="admin/article" element={<ArticleForm />} />
+              <Route path="admin/member" element={<MemberForm />} />
+              <Route path="admin/minute" element={<MinuteForm />} />
+              <Route path="admin/event" element={<EventForm />} />
             </Route>
-
-            <Route path="concerts">
-              <Route index element={<ConcertsHome />} />
-              <Route path=":city" element={<City />} />
-              <Route path="trending" element={<Trending />} />
-            </Route> */}
-
-
-            </Routes>
-          </div>
+          </Routes>
         </div>
-        <Footer />
-      </LoginProvider>
+      </div>
+      <Footer />
+    </LoginProvider>
   );
 }
 
