8755d44b0eebde8d100235df59ba7972ba45bf61
Ashmit Khadka <a0khadka@gmail.com>
2025-06-21 16:31:21 +0100

    updated footer

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,26 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Button = ({ 
-  children, 
-  onClick, 
-  variant = 'default', 
+const Button = ({
+  children,
+  onClick,
+  variant = 'default',
   size = 'medium',
   disabled = false,
   className = '',
-  type = 'button'
+  type = 'button',
+  link, // React Router link
+  href, // External link
 }) => {
-  const baseStyles = 'font-bold rounded transition-colors duration-200 flex items-center justify-center w-max';
-  
-  // const variants = {
-  //   primary: 'bg-blue-500 hover:bg-blue-700 text-white',
-  //   secondary: 'bg-gray-500 hover:bg-gray-700 text-white',
-  //   outline: 'border-2 border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white'
-  // };
+  const baseStyles = 'font-bold rounded transition-colors duration-200 flex items-center justify-center w-max text-decoration-none';
 
   const sizes = {
     small: 'px-3 py-1 text-sm',
     medium: 'px-4 py-2',
-    large: 'px-6 py-3 text-lg'
+    large: 'px-6 py-3 text-lg',
   };
 
   const styles = {
@@ -28,47 +25,54 @@ const Button = ({
       backgroundColor: '#285F9E',
       color: 'white',
       transition: 'background-color 0.2s ease',
-      ':hover': {
-        backgroundColor: '#F56565' // Lighter shade of primary color
-      }
     },
     red: {
       backgroundColor: '#F56565',
       color: 'white',
       transition: 'background-color 0.2s ease',
-      ':hover': {
-        backgroundColor: '#410A0B' // Darker shade of primary color
-      }
-    },
-    darkRed: {
-      backgroundColor: '#410A0B',
-      color: 'white',
-      transition: 'background-color 0.2s ease',
-      ':hover': {
-        backgroundColor: '#924546' // Darker shade of primary color
-      }
     },
     darkBlue: {
       backgroundColor: '#0C1B33',
       color: 'white',
       transition: 'background-color 0.2s ease',
-      ':hover': {
-        backgroundColor: '#285F9E' // Darker shade of primary color
-      }
     },
+  };
+
+  if (link) {
+    // Use React Router's Link
+    return (
+      <Link
+        to={link}
+        className={`${baseStyles} ${sizes[size]} ${className}`}
+        style={styles[variant]}
+      >
+        {children}
+      </Link>
+    );
   }
 
+  if (href) {
+    // Use an anchor tag for external links
+    return (
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={`${baseStyles} ${sizes[size]} ${className}`}
+        style={styles[variant]}
+      >
+        {children}
+      </a>
+    );
+  }
+
+  // Default button
   return (
     <button
       type={type}
       onClick={onClick}
       disabled={disabled}
-      className={`
-        ${baseStyles}
-        ${sizes[size]}
-        ${disabled ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}
-        ${className}
-      `}
+      className={`${baseStyles} ${sizes[size]} ${className}`}
       style={styles[variant]}
     >
       {children}
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,28 +9,28 @@ const Footer = props => {
         <div className='bg-themeDark text-white p-4 pt-8 pb-32 mt-24 w-full flex justify-center text-center'>
             <div className='w-page'>
                 <div className='py-4 flex justify-between flex-wrap gap-4'>
-                    <Button as={Link} className='min-w-[100px] max-w-[200px]' to='/category/type=aid'>Home</Button>
-                    <Button as={Link} className='min-w-[100px] max-w-[200px]' to='/category/type=event'>Events</Button>
-                    <Button as={Link} className='min-w-[100px] max-w-[200px]' to='/category/type=project'>Projects</Button>
-                    <Button as={Link} className='min-w-[100px] max-w-[200px]' to='/advertise'>Committee</Button>
-                    <Button as="a" className='min-w-[100px] max-w-[200px]' href={DocGlance}>About Us</Button>
+                    <Button className='min-w-[100px] max-w-[200px]' link='/'>Home</Button>
+                    <Button className='min-w-[100px] max-w-[200px]' link='/events'>Events</Button>
+                    <Button className='min-w-[100px] max-w-[200px]' link='/projects'>Projects</Button>
+                    <Button className='min-w-[100px] max-w-[200px]' link='/committee'>Committee</Button>
+                    <Button className='min-w-[100px] max-w-[200px]' link='/about'>About Us</Button>
                 </div>
                 <p className='footer__description pt-6'>PNS is committed in helping its members to integrate better with the core British values, while preserving their own culture, faith and religion, and bringing in richness of Nepalese culture to Britain. It also aims to conduct small-scale charity work in Nepal, in particular, helping under-privileged and socially deprived children.</p>
                 <h3 className='font-lora pt-6 pb-4'>Useful Links</h3>
                 <div className='flex justify-center flex-wrap gap-4'>
-                    <Button as="a" className='min-w-[100px] max-w-[200px]' href='https://www.gov.uk/government/organisations/uk-visas-and-immigration'>Home Office UK Visas & Immigration</Button>
-                    <Button as="a" className='min-w-[100px] max-w-[200px]' href='https://www.gov.uk/government/organisations/charity-commission'>Charity Commision UK</Button>
-                    <Button as="a" className='min-w-[100px] max-w-[200px]' href='https://www.cambs.police.uk/report/report'>Cambridgeshire Constabulary</Button>
-                    <Button as="a" className='min-w-[100px] max-w-[200px]' href='http://www.nepalisamajuk.com/component/weblinks/76-nsuk-useful-links/42-uk-visa-application-centre-ktm.html'>UK Visa Application Centre KTM</Button>
-                    <Button as="a" className='min-w-[100px] max-w-[200px]' href='http://www.nepalisamajuk.com/component/weblinks/76-nsuk-useful-links/47-work-permit.html'>Work Permit</Button>
-                    <Button as="a" className='min-w-[100px] max-w-[200px]' href='https://www.citizensadvice.org.uk/'>Citizens Advice</Button>
-                    <Button as="a" className='min-w-[100px] max-w-[200px]' href='http://www.nepalisamajuk.com/component/weblinks/76-nsuk-useful-links/17-foreign-travel-office-nepal.html'>Foreign Travel Advice - Nepal</Button>
-                    <Button as="a" className='min-w-[100px] max-w-[200px]' href='https://www.peterborough.gov.uk/'>Peterborough City Council</Button>
-                    <Button as="a" className='min-w-[100px] max-w-[200px]' href='http://www.nepalimmigration.gov.np/'>Department of Immigration</Button>
-                    <Button as="a" className='min-w-[100px] max-w-[200px]' href='http://www.nepalisamajuk.com/component/weblinks/76-nsuk-useful-links/19-hm-revenue-a-customs.html'>HM Revenue & Customs</Button>
-                    <Button as="a" className='min-w-[100px] max-w-[200px]' href='https://www.nwangliaft.nhs.uk/our-hospitals/peterborough-city-hospital/'>Peterborough City Hospital</Button>
-                    <Button as="a" className='min-w-[100px] max-w-[200px]' href='http://www.nepalisamajuk.com/component/weblinks/76-nsuk-useful-links/21-identity-a-passport-service.html'>HM Passport Office</Button>
-                    <Button as="a" className='min-w-[100px] max-w-[200px]' href='https://uk.nepalembassy.gov.np/'>Embassy of Nepal, London</Button>
+                    <Button className='min-w-[100px] max-w-[200px]' href='https://www.gov.uk/government/organisations/uk-visas-and-immigration'>Home Office UK Visas & Immigration</Button>
+                    <Button className='min-w-[100px] max-w-[200px]' href='https://www.gov.uk/government/organisations/charity-commission'>Charity Commision UK</Button>
+                    <Button className='min-w-[100px] max-w-[200px]' href='https://www.cambs.police.uk/report/report'>Cambridgeshire Constabulary</Button>
+                    <Button className='min-w-[100px] max-w-[200px]' href='http://www.nepalisamajuk.com/component/weblinks/76-nsuk-useful-links/42-uk-visa-application-centre-ktm.html'>UK Visa Application Centre KTM</Button>
+                    <Button className='min-w-[100px] max-w-[200px]' href='http://www.nepalisamajuk.com/component/weblinks/76-nsuk-useful-links/47-work-permit.html'>Work Permit</Button>
+                    <Button className='min-w-[100px] max-w-[200px]' href='https://www.citizensadvice.org.uk/'>Citizens Advice</Button>
+                    <Button className='min-w-[100px] max-w-[200px]' href='http://www.nepalisamajuk.com/component/weblinks/76-nsuk-useful-links/17-foreign-travel-office-nepal.html'>Foreign Travel Advice - Nepal</Button>
+                    <Button className='min-w-[100px] max-w-[200px]' href='https://www.peterborough.gov.uk/'>Peterborough City Council</Button>
+                    <Button className='min-w-[100px] max-w-[200px]' href='http://www.nepalimmigration.gov.np/'>Department of Immigration</Button>
+                    <Button className='min-w-[100px] max-w-[200px]' href='http://www.nepalisamajuk.com/component/weblinks/76-nsuk-useful-links/19-hm-revenue-a-customs.html'>HM Revenue & Customs</Button>
+                    <Button className='min-w-[100px] max-w-[200px]' href='https://www.nwangliaft.nhs.uk/our-hospitals/peterborough-city-hospital/'>Peterborough City Hospital</Button>
+                    <Button className='min-w-[100px] max-w-[200px]' href='http://www.nepalisamajuk.com/component/weblinks/76-nsuk-useful-links/21-identity-a-passport-service.html'>HM Passport Office</Button>
+                    <Button className='min-w-[100px] max-w-[200px]' href='https://uk.nepalembassy.gov.np/'>Embassy of Nepal, London</Button>
                 </div>
                 {/* <div className='footer__link footer__link--credit margin-top-medium'>                    
                     <a href="https://www.linkedin.com/in/ashmit-khadka/">Website Created by Ashmit Khadka {<IconLinkedIn/>}</a>
